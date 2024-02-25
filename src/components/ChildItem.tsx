aefcb9425e51fa6b6caac52c1628046926eeb885
tianBreznik <tian.breza@gmail.com>
2024-02-25 15:47:46 +0100

    data fetch component sorted

--- src/components/ChildItem.tsx
+++ src/components/ChildItem.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react'
 
-export default function Child({parentToChildItem}) {
+type dataType = {
+   userId: number;
+   id: number;
+   title: string;
+   body: string;
+   onDelete: (id: number) => void;
+}
+
+export default function ChildItem({parentToChildItem}) {
+
+    const [data, setData] = useState<dataType[]>([]);
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    useEffect(() => {
+       fetch(url).then(result => result.json()).then(alldata => setData(alldata)).catch(error => console.log(error.message))
+    }, [])
+    console.log(parentToChildItem);
+
     return (
         <div>
             {parentToChildItem}
+            {data.map(info => (
+                <div key={info.id}>{info.title}</div>
+            ))}
         </div>
     )
 }
