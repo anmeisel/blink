e3f2417aafaa09ee8e9f0c8f3edc8dbbf3ce21c2
tianBreznik <tian.breza@gmail.com>
2024-02-25 17:16:28 +0100

    keywords extracted from news descriptions

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,11 +1,19 @@
 import { type PropsWithChildren } from "react"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import FetchNews from './FetchNews.tsx'
 import FetchGifs from './FetchGifs.tsx'
 import FetchData from './FetchData.tsx'
 import ChildItem from './ChildItem.tsx';
+const keyword_extractor = require("keyword-extractor");
 
+
+type dataType = {
+    publishedAt: string;
+    title: string;
+    description: string;
+    onDelete: (id: number) => void;
+}
 
 type ItemProps = PropsWithChildren<{ 
     id: number;
@@ -13,22 +21,44 @@ type ItemProps = PropsWithChildren<{
     onDelete: (id: number) => void;
 }>
 
+const keywords: any[] = [];
+
 
 export default function Item({ info, id, children, onDelete }: ItemProps) {
-    const [data, setData] = useState('');
+    const [data, setData] = useState<dataType[]>([]);
+    const apikey = '48878147a05368955e614dca472e6dc4';
+    const category = 'world';
+    const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=50&apikey=' + apikey;
+    useEffect(() => {
+     fetch(url).then(result => result.json()).then(alldata => setData(alldata.articles)).catch(error => console.log(error.message))
+    }, [])
 
-    const parentToChildItem = () => {
-        setData("This is data from Parent Component to the Child Component.");
-      }
+    data.forEach(function(obj) { 
+        const extraction_result = keyword_extractor.extract(obj.description,{
+            language:"english",
+            remove_digits: true,
+            return_changed_case:true,
+            return_chained_words: true,
+            remove_duplicates: true
+        });
+        keywords.push(extraction_result);
+    });
+
+
+    console.log(keywords);
+
+    // const parentToChildItem = () => {
+    //     setData("This is data from Parent Component to the Child Component.");
+    //   }
     return (
     <article>
         <div>
             <h2>{info}</h2>
             {children}
-            <ChildItem parentToChildItem={data}/>
+            {/* <ChildItem parentToChildItem={data}/> */}
             {/* <FetchNews/> */}
             {/* <FetchGifs/> */}
-            <Button primary onClick={() => parentToChildItem()}>Click Parent</Button>
+            {/* <Button primary onClick={() => parentToChildItem()}>Click Parent</Button> */}
         </div>
         <button onClick={() => onDelete(id)}>Delete</button>
     </article>
